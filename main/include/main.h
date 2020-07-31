6b47261e6c6a5105fccac8d9327e2ca31a36a018
Marek Ištok <istok@infotech.sk>
2020-07-31 09:40:46 +0200

    Initial commit

---
+++ main/include/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+extern "C" {
+  void app_main(void);
+}
+
+void app_main(void);
+
+#endif /* MAIN_H */
---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <main.h>
+
+#define LOG_LOCAL_LEVEL ESP_LOG_DEBUG
+
+static const char* TAG = "main";
+
+void app_main(void) {
+
+}
